aef1f58b91b2e04afdf1e60d533daeaa6b11ab03
gouravsaini546 <115344340+gouravsaini546@users.noreply.github.com>
2023-04-06 14:35:37 +0530

    test login

---
+++ login.py
@@ -0,0 +1,60 @@
+import streamlit as st
+import snowflake.connector
+import hashlib
+
+def connect_to_snowflake():
+  conn = snowflake.connector.connect(**streamlit.secrets[ "snowflake" ])
+  return conn
+def create_new_user_profile(name, email, fats, carbohydrates, protein):
+  conn = connect_to_snowflake()
+  cursor = conn.cursor()
+  cursor.execute("INSERT INTO user_profiles (name, email, fats, carbohydrates, protein) VALUES (%s, %s, %s, %s, %s)", (name, email, fats, carbohydrates, protein))
+  conn.commit()
+  cursor.close()
+  conn.close()
+def create_new_user_profile(name, email, fats, carbohydrates, protein):
+  conn = connect_to_snowflake()
+  cursor = conn.cursor()
+  cursor.execute("INSERT INTO user_profiles (name, email, fats, carbohydrates, protein) VALUES (%s, %s, %s, %s, %s)", (name, email, fats, carbohydrates, protein))
+  conn.commit()
+  cursor.close()
+  conn.close()
+
+def authenticate_user_login(email, password):
+  conn = connect_to_snowflake()
+  cursor = conn.cursor()
+  cursor.execute("SELECT password_hash FROM user_profiles WHERE email = %s", (email,))
+  result = cursor.fetchone()
+  cursor.close()
+  conn.close()
+  if result is not None:
+    stored_hash = result[0]
+    input_hash = hashlib.sha256(password.encode()).hexdigest()
+    if stored_hash == input_hash:
+      return True
+  return False
+
+st.sidebar.header('Navigation')
+page = st.sidebar.radio('Go to', ['Create Profile', 'Login'])
+
+
+if page == 'Create Profile':
+    st.header('Create Your Profile')
+    name = st.text_input('Name')
+    email = st.text_input('Email')
+    fats = st.number_input('Fats', min_value=0, max_value=100)
+    carbohydrates = st.number_input('Carbohydrates', min_value=0, max_value=100)
+    protein = st.number_input('Protein', min_value=0, max_value=100)
+    if st.button('Create Profile'):
+        create_new_user_profile(name, email, fats, carbohydrates, protein)
+        st.success('Profile created successfully!')
+        
+if page == 'Login':
+    st.header('Login')
+    email = st.text_input('Email')
+    password = st.text_input('Password', type='password')
+    if st.button('Login'):
+        if authenticate_user_login(email, password):
+            st.success('Login successful
+
+
