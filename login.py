9fd879fdbb11e110a6cf5723203c52f6b951ea48
gouravsaini546 <115344340+gouravsaini546@users.noreply.github.com>
2023-04-07 14:07:26 +0530

    Update login.py

--- login.py
+++ login.py
@@ -2,17 +2,18 @@ import streamlit as st
 import snowflake.connector
 import hashlib
 
-st.title('Karigari')
+st.set_page_config(page_title="Karigari", page_icon=":fork_and_knife:")
 
 def connect_to_snowflake():
   conn = snowflake.connector.connect(**st.secrets[ "snowflake" ])
   return conn
-def create_new_user_profile(name, email, password, age, weight, height, activity_level):
+
+def create_new_user_profile(name, email, password):
     # Hash the password using SHA-256 algorithm
   password_hash = hashlib.sha256(password.encode()).hexdigest()
   conn = connect_to_snowflake()
   cursor = conn.cursor()
-  cursor.execute("INSERT INTO user_profiles (name, email, password_hash, age, weight, height, activity_level) VALUES (%s, %s, %s, %s, %s, %s, %s)", (name, email, password_hash, age, weight, height, activity_level))
+  cursor.execute("INSERT INTO user_profiles (name, email, password_hash) VALUES (%s, %s, %s)", (name, email, password_hash))
   conn.commit()
   cursor.close()
   conn.close()
@@ -31,30 +32,60 @@ def authenticate_user_login(email, password):
       return True
   return False
 
-st.sidebar.header('Navigation')
-page = st.sidebar.radio('Go to', ['Create Profile', 'Login'])
+def get_user_data(email):
+    conn = connect_to_snowflake()
+    cursor = conn.cursor()
+    cursor.execute("SELECT name, email, age, weight, height, activity_level FROM user_profiles WHERE email = %s", (email,))
+    result = cursor.fetchone()
+    cursor.close()
+    conn.close()
+    return result
 
+def get_user_favourites(email):
+    conn = connect_to_snowflake()
+    cursor = conn.cursor()
+    cursor.execute("SELECT recipe_name FROM user_favourites WHERE email = %s", (email,))
+    results = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return [result[0] for result in results]
+
+def add_user_favourite(email, recipe_name):
+    conn = connect_to_snowflake()
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO user_favourites (email, recipe_name) VALUES (%s, %s)", (email, recipe_name))
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+st.sidebar.header('Navigation')
+page = st.sidebar.radio('Go to', ['Create Profile', 'Login', 'Dashboard'])
 
 if page == 'Create Profile':
-    st.header('Create Your Profile')
+    st.title('Create Your Profile')
     name = st.text_input('Name')
     email = st.text_input('Email')
     password = st.text_input('Password', type='password')
-    age = st.number_input('Age(In Years)', min_value=0, max_value=200)
-    weight = st.number_input('Weight(In KG)', min_value=0, max_value=1000)
-    height = st.number_input('Height(In CM)', min_value=0, max_value=500)
-    activity_level = st.selectbox('Activity Level', options=['Sedentary',  'Moderately Active', 'Very Active'])
     if st.button('Create Profile'):
-        create_new_user_profile(name, email, password, age, weight, height, activity_level)
+        create_new_user_profile(name, email, password)
         st.success('Profile created successfully!')
-        
-if page == 'Login':
-    st.header('Login')
+
+elif page == 'Login':
+    st.title('Login')
     email = st.text_input('Email')
     password = st.text_input('Password', type='password')
     if st.button('Login'):
         if authenticate_user_login(email, password):
             st.success('Login successful!')
+            st.session_state.logged_in = True
+            st.session_state.email = email
         else:
             st.error('Incorrect email or password. Please try again.')
 
+if st.session_state.get('logged_in'):
+    st.title('Dashboard')
+    st.write(f"Welcome {get_user_data(st.session_state.email)[0]}!")
+    st.write(f"Email: {get_user_data(st.session_state.email)[1]}")
+    st.write(f"Age: {get_user_data(st.session_state.email)[2]}")
+    st.write(f"Weight: {get_user_data(st.session_state.email)[3]}")
+    st.write(f"Height: {get_user_data(st.session_state
