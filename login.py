79eddedd98d85c20c2522c6eb055d95241b6598b
gouravsaini546 <115344340+gouravsaini546@users.noreply.github.com>
2023-04-07 10:28:43 +0530

    Update login.py

--- login.py
+++ login.py
@@ -1,10 +1,7 @@
 import streamlit as st
 import snowflake.connector
 import hashlib
-
 # import necessary packages
-import streamlit as st
-import snowflake.connector
 import requests
 
 # initialize the Streamlit app
@@ -12,10 +9,40 @@ st.set_page_config(page_title='Karigari - Evolving Food Industry', page_icon=':a
 st.markdown('<style>body{background-color: #e6f7ff;}</style>', unsafe_allow_html=True)
 st.title('Karigari - Evolving Food Industry')
 
-# define the login page
-def login():
+def authenticate_user_login(email, password):
+    conn = connect_to_snowflake()
+    cursor = conn.cursor()
+    cursor.execute("SELECT name, password_hash FROM user_profiles WHERE email=%s", (email,))
+    result = cursor.fetchone()
+    if result is None:
+        return False, None
+    name, password_hash = result
+    entered_password_hash = hashlib.sha256(password.encode()).hexdigest()
+    if password_hash == entered_password_hash:
+        return True, name
+    else:
+        return False, None
+
+
+st.sidebar.header('Navigation')
+page = st.sidebar.radio('Go to', ['Create Profile', 'Login'])
+
+
+if page == 'Create Profile':
+    st.header('Create Your Profile')
+    name = st.text_input('Name')
+    email = st.text_input('Email')
+    password = st.text_input('Password', type='password')
+    fats = st.number_input('Fats', min_value=0, max_value=100)
+    carbohydrates = st.number_input('Carbohydrates', min_value=0, max_value=100)
+    protein = st.number_input('Protein', min_value=0, max_value=100)
+    if st.button('Create Profile'):
+        create_new_user_profile(name, email, password, fats, carbohydrates, protein)
+        st.success('Profile created successfully!')
+        
+if page == 'Login':
     st.header('Login')
-    st.image('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/285/shield_1f6e1.png', width=100)
+    st.image('https://cdn-icons-png.flaticon.com/512/2592/2592317.png', width=100)
     email = st.text_input('Email')
     password = st.text_input('Password', type='password')
     if st.button('Login'):
@@ -27,68 +54,32 @@ def login():
         else:
             st.error('Incorrect email or password. Please try again.')
 
-# define the user dashboard page
-def user_dashboard():
+            
+if page == 'User Dashboard':
     st.header(f'Welcome, {st.session_state["name"]}!')
-    st.image('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/google/298/green-apple_1f34f.png', width=100)
+    st.subheader('Nutrients Selected')
+    st.write(f'Fats: {fats}%')
+    st.write(f'Carbohydrates: {carbohydrates}%')
+    st.write(f'Protein: {protein}%')
+    
+    st.subheader('Favorites')
+    # display the user's favorite recipes
+    
+    st.subheader('Recipe Search Tool')
+    # display a tool to search for recipes based on ingredients using the recipe API
+
+with st.beta_container():
+    st.subheader('Nutrients Selected')
+    st.write(f'Fats: {fats}%')
+    st.write(f'Carbohydrates: {carbohydrates}%')
+    st.write(f'Protein: {protein}%')
+    st.image('https://w7.pngwing.com/pngs/493/176/png-transparent-apple-green-apple-food-image-file-formats-leaf-thumbnail.png', width=100)
     st.write('')
-    with st.beta_container():
-        st.subheader('Nutrients Selected')
-        st.write(f'Fats: {fats}%')
-        st.write(f'Carbohydrates: {carbohydrates}%')
-        st.write(f'Protein: {protein}%')
-    cols = st.beta_columns(2)
-    with cols[0]:
-        st.subheader('Favorites')
-        # display the user's favorite recipes
-    with cols[1]:
-        st.image('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/twitter/282/heart-with-arrow_1f498.png', width=100)
-        st.write('')
-    st.subheader('Search Recipes')
-    # allow the user to search for recipes using the Recipe API
-
-# define a function to authenticate the user login
-def authenticate_user_login(email, password):
-    # authenticate the user using the Snowflake data warehouse
-    # return True and the user's name if successful, False otherwise
-    return True, 'John Doe'
-
-# initialize the session state
-if 'name' not in st.session_state:
-    st.session_state['name'] = None
-
-# create a dictionary to map page names to functions
-pages = {
-    'Login': login,
-    'User Dashboard': user_dashboard
-}
-
-# define the page selection dropdown
-page = st.sidebar.selectbox('Select a page', options=list(pages.keys()))
-
-# display the selected page
-pages[page]()
-
-# redirect the user to the user dashboard after successful login
-if st.session_state['name'] is not None and page == 'Login':
-    st.experimental_rerun()  # this will automatically switch the page to the user dashboard
-
-
-def connect_to_snowflake():
-  conn = snowflake.connector.connect(**st.secrets[ "snowflake" ])
-  return conn
-def create_new_user_profile(name, email, password, fats, carbohydrates, protein):
-    # Hash the password using SHA-256 algorithm
-  password_hash = hashlib.sha256(password.encode()).hexdigest()
-  conn = connect_to_snowflake()
-  cursor = conn.cursor()
-  cursor.execute("INSERT INTO user_profiles (name, email, password_hash, fats, carbohydrates, protein) VALUES (%s, %s, %s, %s, %s, %s)", (name, email, password_hash, fats, carbohydrates, protein))
-  conn.commit()
-  cursor.close()
-  conn.close()
-
-
-
-
-
-
+    
+cols = st.beta_columns(2)
+with cols[0]:
+    st.subheader('Favorites')
+    # display the user's favorite recipes
+with cols[1]:
+   # st.image('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/twitter/282/heart-with-arrow_1f498.png', width=100)
+    st.write('')
