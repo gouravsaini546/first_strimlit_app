2ec533a5c0fc0b9d5692fda8c603602d3a9f07f4
gouravsaini546 <115344340+gouravsaini546@users.noreply.github.com>
2023-04-07 14:13:13 +0530

    Update login.py

--- login.py
+++ login.py
@@ -8,12 +8,12 @@ def connect_to_snowflake():
   conn = snowflake.connector.connect(**st.secrets[ "snowflake" ])
   return conn
 
-def create_new_user_profile(name, email, password):
+def create_new_user_profile(name, email, password, age, weight, height, activity_level):
     # Hash the password using SHA-256 algorithm
   password_hash = hashlib.sha256(password.encode()).hexdigest()
   conn = connect_to_snowflake()
   cursor = conn.cursor()
-  cursor.execute("INSERT INTO user_profiles (name, email, password_hash) VALUES (%s, %s, %s)", (name, email, password_hash))
+  cursor.execute("INSERT INTO user_profiles (name, email, password_hash, age, weight, height, activity_level) VALUES (%s, %s, %s, %s, %s, %s, %s)", (name, email, password_hash, age, weight, height, activity_level))
   conn.commit()
   cursor.close()
   conn.close()
@@ -62,12 +62,16 @@ st.sidebar.header('Navigation')
 page = st.sidebar.radio('Go to', ['Create Profile', 'Login', 'Dashboard'])
 
 if page == 'Create Profile':
-    st.title('Create Your Profile')
+    st.header('Create Your Profile')
     name = st.text_input('Name')
     email = st.text_input('Email')
     password = st.text_input('Password', type='password')
+    age = st.number_input('Age(In Years)', min_value=0, max_value=200)
+    weight = st.number_input('Weight(In KG)', min_value=0, max_value=1000)
+    height = st.number_input('Height(In CM)', min_value=0, max_value=500)
+    activity_level = st.selectbox('Activity Level', options=['Sedentary',  'Moderately Active', 'Very Active'])
     if st.button('Create Profile'):
-        create_new_user_profile(name, email, password)
+        create_new_user_profile(name, email, password, age, weight, height, activity_level)
         st.success('Profile created successfully!')
 
 elif page == 'Login':
