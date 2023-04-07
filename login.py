ba2189b75fd85d806c5820679d3ba99ae644dbec
gouravsaini546 <115344340+gouravsaini546@users.noreply.github.com>
2023-04-07 11:39:17 +0530

    Update login.py

--- login.py
+++ login.py
@@ -1,48 +1,39 @@
 import streamlit as st
 import snowflake.connector
 import hashlib
-# import necessary packages
-import requests
 
-# initialize the Streamlit app
-st.set_page_config(page_title='Karigari - Evolving Food Industry', page_icon=':apple:', layout='wide', initial_sidebar_state='auto')
-st.markdown('<style>body{background-color: #e6f7ff;}</style>', unsafe_allow_html=True)
 st.title('Karigari - Evolving Food Industry')
 
-def authenticate_user_login(email, password):
-    conn = connect_to_snowflake()
-    cursor = conn.cursor()
-    cursor.execute("SELECT name, password_hash FROM user_profiles WHERE email=%s", (email,))
-    result = cursor.fetchone()
-    if result is None:
-        return False, None
-    name, password_hash = result
-    entered_password_hash = hashlib.sha256(password.encode()).hexdigest()
-    if password_hash == entered_password_hash:
-        return True, name
-    else:
-        return False, None
+def connect_to_snowflake():
+  conn = snowflake.connector.connect(**st.secrets[ "snowflake" ])
+  return conn
+def create_new_user_profile(name, email, password, fats, carbohydrates, protein):
+    # Hash the password using SHA-256 algorithm
+  password_hash = hashlib.sha256(password.encode()).hexdigest()
+  conn = connect_to_snowflake()
+  cursor = conn.cursor()
+  cursor.execute("INSERT INTO user_profiles (name, email, password_hash, fats, carbohydrates, protein) VALUES (%s, %s, %s, %s, %s, %s)", (name, email, password_hash, fats, carbohydrates, protein))
+  conn.commit()
+  cursor.close()
+  conn.close()
 
+def authenticate_user_login(email, password):
+  conn = connect_to_snowflake()
+  cursor = conn.cursor()
+  cursor.execute("SELECT password_hash FROM user_profiles WHERE email = %s", (email,))
+  result = cursor.fetchone()
+  cursor.close()
+  conn.close()
+  if result is not None:
+    stored_hash = result[0]
+    input_hash = hashlib.sha256(password.encode()).hexdigest()
+    if stored_hash == input_hash:
+      return True
+  return False
 
 st.sidebar.header('Navigation')
-page = st.sidebar.radio('Go to', ['Create Profile', 'Login','User Dashboard'])
+page = st.sidebar.radio('Go to', ['Create Profile', 'Login'])
 
-def user_dashboard():
-    st.header(f'Welcome, {st.session_state["name"]}!')
-    st.image('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/google/298/green-apple_1f34f.png', width=100)
-    st.write('')
-    with st.beta_container():
-        st.subheader('Nutrients Selected')
-        st.write(f'Fats: {fats}%')
-        st.write(f'Carbohydrates: {carbohydrates}%')
-        st.write(f'Protein: {protein}%')
-    cols = st.beta_columns(2)
-    with cols[0]:
-        st.subheader('Favorites')
-        # display the user's favorite recipes
-    with cols[1]:
-        st.write('')
-    st.subheader('Search Recipes')
 
 if page == 'Create Profile':
     st.header('Create Your Profile')
@@ -58,56 +49,11 @@ if page == 'Create Profile':
         
 if page == 'Login':
     st.header('Login')
-    st.image('https://cdn-icons-png.flaticon.com/512/2592/2592317.png', width=100)
     email = st.text_input('Email')
     password = st.text_input('Password', type='password')
     if st.button('Login'):
-        success, name = authenticate_user_login(email, password)
-        if success:
-            st.success(f'Welcome back, {name}!')
-            st.session_state['name'] = name
-            st.experimental_rerun()
+        if authenticate_user_login(email, password):
+            st.success('Login successful!')
         else:
             st.error('Incorrect email or password. Please try again.')
 
-            
-if page == 'User Dashboard':
-    st.header(f'Welcome, {st.session_state["name"]}!')
-    st.subheader('Nutrients Selected')
-    st.write(f'Fats: {fats}%')
-    st.write(f'Carbohydrates: {carbohydrates}%')
-    st.write(f'Protein: {protein}%')
-    
-    st.subheader('Favorites')
-    # display the user's favorite recipes
-    
-    st.subheader('Recipe Search Tool')
-    # display a tool to search for recipes based on ingredients using the recipe API
-
-with st.container():
-    st.subheader('Nutrients Selected')
-    st.write(f'Fats: {fats}%')
-    st.write(f'Carbohydrates: {carbohydrates}%')
-    st.write(f'Protein: {protein}%')
-    st.image('https://w7.pngwing.com/pngs/493/176/png-transparent-apple-green-apple-food-image-file-formats-leaf-thumbnail.png', width=100)
-    st.write('')
-    
-cols = st.beta_columns(2)
-with cols[0]:
-    st.subheader('Favorites')
-    # display the user's favorite recipes
-with cols[1]:
-   # st.image('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/twitter/282/heart-with-arrow_1f498.png', width=100)
-    st.write('')
-def connect_to_snowflake():
-  conn = snowflake.connector.connect(**st.secrets[ "snowflake" ])
-  return conn
-def create_new_user_profile(name, email, password, fats, carbohydrates, protein):
-    # Hash the password using SHA-256 algorithm
-  password_hash = hashlib.sha256(password.encode()).hexdigest()
-  conn = connect_to_snowflake()
-  cursor = conn.cursor()
-  cursor.execute("INSERT INTO user_profiles (name, email, password_hash, fats, carbohydrates, protein) VALUES (%s, %s, %s, %s, %s, %s)", (name, email, password_hash, fats, carbohydrates, protein))
-  conn.commit()
-  cursor.close()
-  conn.close()
