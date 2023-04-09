94be9811c301fd98801a9a20c668f45128de4e9e
gouravsaini546 <115344340+gouravsaini546@users.noreply.github.com>
2023-04-10 00:51:48 +0530

    Update login.py

--- login.py
+++ login.py
@@ -156,7 +156,7 @@ if st.session_state.get('logged_in'):
             st.write(f"BMI: {get_user_data(st.session_state.email)[7]}")
             st.write(f"Activity Level: {get_user_data(st.session_state.email)[6]}")
     rows = get_user_favourites(get_user_data(st.session_state.email)[1])
-    print(rows.shape)
+    print(rows)
 
     #df3 = pd.DataFrame(rows, columns=["Type", "Title", "Topping", "Calories", "Protein", "Fat", "Sodium"])
     # Hide nutritional information by default
