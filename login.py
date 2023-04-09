df9dd2672e0b1b19235898fa3974ace4fde43e8a
gouravsaini546 <115344340+gouravsaini546@users.noreply.github.com>
2023-04-09 23:34:17 +0530

    Update login.py

--- login.py
+++ login.py
@@ -188,7 +188,7 @@ if st.session_state.get('logged_in'):
     # Display the total nutrient amounts in a table
     #st.table(total_df['Amount'])
     st.table(total_food_df)
-    st.table(total_food_df['Nutrient'])
+    st.table(total_food_df['Amount']['Calories'])
     #if st.button("Save as Favorites"):
       #favorites = (email,selected_food_type,selected_food_item,selected_toppings
 
