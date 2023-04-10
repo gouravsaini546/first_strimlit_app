39a046e37f09aefdbf8535c5a3886c6e23f891d2
gouravsaini546 <115344340+gouravsaini546@users.noreply.github.com>
2023-04-11 00:30:37 +0530

    Update login.py

--- login.py
+++ login.py
@@ -4,18 +4,18 @@ import hashlib
 import pandas as pd
 #from streamlit import theming
 
-primary_color = "#7e57c2"
+#primary_color = "#7e57c2"
 
 # Set theme style
-theme = {
-    "primaryColor": primary_color,
-    "secondaryBackgroundColor": "#f9f9f9",
-    "textColor": "#333333",
-    "font": "sans serif",
-}
+#theme = {
+    #"primaryColor": primary_color,
+   # "secondaryBackgroundColor": "#f9f9f9",
+  #  "textColor": "#333333",
+ #   "font": "sans serif",
+#}
 
 # Apply the theme
-theming.set_theme(theme)
+#theming.set_theme(theme)
 
 st.set_page_config(page_title="Karigari", page_icon=":fork_and_knife:")
 
