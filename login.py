d4b7c636232b0347ec84245dc39ced926569bc09
gouravsaini546 <115344340+gouravsaini546@users.noreply.github.com>
2023-04-10 10:11:58 +0530

    Update login.py

--- login.py
+++ login.py
@@ -56,7 +56,7 @@ def get_user_favourites(email):
     results = cursor.fetchall()
     cursor.close()
     conn.close()
-    return result
+    return results
 
 def add_user_favourite(email, recipe_name):
     conn = connect_to_snowflake()
